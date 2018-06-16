3991079a93d464658367ebc7a69c0f8765aa19af
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-16 21:10:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -96,4 +96,15 @@ class TicTacToe
     end
     return nil
   end
+  
+  def play
+    until over?
+      turn
+    end 
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
 end
