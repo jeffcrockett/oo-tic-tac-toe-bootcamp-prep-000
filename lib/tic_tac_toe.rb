2308d7bfb68f0486ccf8d1ebee9c84b457a71fbc
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-16 20:56:57 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,5 +20,8 @@ class TicTacToe
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-end
+  end
+  
+  def input_to_index(input)
+    input.to_i - 1
 end
