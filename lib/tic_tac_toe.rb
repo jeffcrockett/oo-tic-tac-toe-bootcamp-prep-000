f206c521ff5bd3fd63f79b159022ed916fd23a67
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-16 20:53:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -5,6 +5,9 @@ class TicTacToe
     WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                     [0, 3, 6], [1, 4, 7], [2, 5, 8],
                     [0, 4, 8], [2, 4, 6]]
+  def board
+    @board
+  end
   def position_taken?(index)
     !(self.board[index].nil? || self.board[index] == " ")
   end
